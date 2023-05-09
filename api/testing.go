ca1ff84b8a6014cac3985499c40e3d2e3a638b2a
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-05-09 19:01:40 -0400

    Content type maybe?

--- api/testing.go
+++ api/testing.go
@@ -6,5 +6,6 @@ import (
 )
 
 func HandleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(w, "<marquee><h1>Hello from Go!!!</h1></marquee>")
 }
